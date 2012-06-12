12e3b7282c3ea0494ddc39fcd6a0797b032fc2c6
Robert Impey <robert@impey.info>
2012-06-12 19:28:33 +0100

    Printing the output of the function.

--- PracticalGeneticAlgorithms/Module1.vb
+++ PracticalGeneticAlgorithms/Module1.vb
@@ -1,7 +1,39 @@
 ﻿Module Module1
 
     Sub Main()
+        If My.Application.CommandLineArgs.Count > 0 Then
+            Dim Tool = My.Application.CommandLineArgs(0)
 
+            Select Case Tool
+                Case "Print"
+                    Const XMin = 0.0
+                    Const XMax = 10.0
+                    Const XStep = 0.1
+
+                    Const YMin = 0.0
+                    Const YMax = 10.0
+                    Const YStep = 0.1
+
+                    Const Separator = ","
+
+                    Dim X = XMin
+                    While X <= XMax
+                        Dim Y = YMin
+                        While Y <= YMax
+                            Console.Write(F(X, Y) & Separator)
+                            Y += YStep
+                        End While
+                        X += XStep
+                        Console.WriteLine()
+                    End While
+            End Select
+        Else
+            Console.WriteLine("Tell me what to do!")
+        End If
     End Sub
+
+    Function F(ByVal X As Double, ByVal Y As Double) As Double
+        Return (X * Math.Sin(4 * X)) + (1.1 * Y * Math.Sin(2 * Y))
+    End Function
 
 End Module
