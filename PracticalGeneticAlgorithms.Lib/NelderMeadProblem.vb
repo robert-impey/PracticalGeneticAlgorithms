c39368c9deadea6d52e979f0988768176e27a8a9
Robert Impey <robert@impey.info>
2012-06-13 16:35:37 +0100

    Comment to explain the function we are trying to minimise.

--- PracticalGeneticAlgorithms.Lib/NelderMeadProblem.vb
+++ PracticalGeneticAlgorithms.Lib/NelderMeadProblem.vb
@@ -19,6 +19,13 @@
         End While
     End Sub
 
+    ''' <summary>
+    ''' This is the function for whose minimum we are searching.
+    ''' </summary>
+    ''' <param name="X"></param>
+    ''' <param name="Y"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
     Function F(ByVal X As Double, ByVal Y As Double) As Double
         Return (X * Math.Sin(4 * X)) + (1.1 * Y * Math.Sin(2 * Y))
     End Function
