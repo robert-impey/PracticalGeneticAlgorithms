90d5a4ddce8ae3648d20c9450375633c350e3e7d
Robert Impey <robert@impey.info>
2013-09-28 11:45:39 +0100

    Separate assert statements into different tests.

--- PracticalGeneticAlgorithms.Test/MathsHelpersTest.vb
+++ PracticalGeneticAlgorithms.Test/MathsHelpersTest.vb
@@ -2,13 +2,20 @@
 Imports PracticalGeneticAlgorithms.Lib
 
 <TestFixture()> Public Class MathsHelpersTest
-    <Test()> Public Sub RandomDoublesAreInRange()
-        Dim Min = 100.0
-        Dim Max = 200.0
+    Private Const min = 100.0
+    Private Const max = 200.0
 
-        Dim Result = MathsHelpers.RandomInRange(Min, Max)
+    Private Property Result As Double
 
-        Assert.GreaterOrEqual(Result, Min)
-        Assert.LessOrEqual(Result, Max)
+    <SetUp()> Public Sub SetUp()
+        Result = MathsHelpers.RandomInRange(min, max)
+    End Sub
+
+    <Test()> Public Sub RandomDoublesAreBigEnough()
+        Assert.GreaterOrEqual(Result, min)
+    End Sub
+
+    <Test()> Public Sub RandomDoublesAreSmallEnough()
+        Assert.LessOrEqual(Result, max)
     End Sub
 End Class
