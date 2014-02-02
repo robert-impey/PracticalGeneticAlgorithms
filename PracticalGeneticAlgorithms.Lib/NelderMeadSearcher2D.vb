00d254b81ea8b150b00bcfbbe7e22eee9766aa7f
Robert Impey <robert@impey.info>
2014-02-02 23:46:25 +0000

    Testing that simplex points are created in range.

--- PracticalGeneticAlgorithms.Lib/NelderMeadSearcher2D.vb
+++ PracticalGeneticAlgorithms.Lib/NelderMeadSearcher2D.vb
@@ -2,12 +2,21 @@
     Inherits Searcher2D
 
     Public Overrides Function Solve() As Solution2D
-        ' Create a random simplex
-        Dim Simplex = New Simplex
+        Dim simplex = CreateSimplex()
+        Return Simplex.BestSolution
+    End Function
+
+    ''' <summary>
+    ''' Creates a random simplex in range for the searcher.
+    ''' </summary>
+    ''' <returns>A random simplex</returns>
+    ''' <remarks></remarks>
+    Public Function CreateSimplex() As Simplex
+        Dim simplex = New Simplex
         For I = 1 To 3
-            Simplex.Solutions.Add(New Solution2D(New Point2D(MathsHelpers.RandomInRange(XMin, XMax), _
+            simplex.Solutions.Add(New Solution2D(New Point2D(MathsHelpers.RandomInRange(XMin, XMax), _
                                    MathsHelpers.RandomInRange(YMin, YMax)), Me))
         Next
-        Return Simplex.BestSolution
+        Return simplex
     End Function
 End Class
---
+++ PracticalGeneticAlgorithms.Test/NelderMeadTests/GivenA2dSearcherWhenASimplexIsCreated.vb
@@ -0,0 +1,55 @@
+﻿Imports NUnit.Framework
+Imports PracticalGeneticAlgorithms.Lib
+
+<TestFixture()>
+Public Class GivenA2dSearcherWhenASimplexIsCreated
+    Inherits AssertionHelper
+
+    Private Const XMin = 0.0
+    Private Const XMax = 10.0
+    Private Const YMin = 0.0
+    Private Const YMax = 10.0
+
+    Private Simplex As Simplex
+    Private Searcher As NelderMeadSearcher2D
+
+    <SetUp()> Public Sub SetUp()
+        Searcher = New NelderMeadSearcher2D
+
+        With Searcher
+            .XMin = XMin
+            .XMax = XMax
+            .YMin = YMin
+            .YMax = YMax
+
+            .F = AddressOf F_1_1
+        End With
+
+        Simplex = Searcher.CreateSimplex()
+    End Sub
+
+    <TearDown()> Public Sub TearDown()
+        Simplex = Nothing
+        Searcher = Nothing
+    End Sub
+
+    <Test>
+    Public Sub ThenTheXValueOfTheBestSolultionIsGreaterThanOrEqualToTheMin()
+        Expect(Simplex.BestSolution.P.X, [Is].GreaterThanOrEqualTo(XMin))
+    End Sub
+
+    <Test>
+    Public Sub ThenTheXValueOfTheBestSolultionIsLessThanTheMax()
+        Expect(Simplex.BestSolution.P.X, [Is].LessThan(XMax))
+    End Sub
+
+    <Test>
+    Public Sub ThenTheYValueOfTheBestSolultionIsGreaterThanOrEqualToTheMin()
+        Expect(Simplex.BestSolution.P.Y, [Is].GreaterThanOrEqualTo(YMin))
+    End Sub
+
+    <Test>
+    Public Sub ThenTheYValueOfTheBestSolultionIsLessThanMax()
+        Expect(Simplex.BestSolution.P.Y, [Is].LessThan(YMax))
+    End Sub
+End Class
